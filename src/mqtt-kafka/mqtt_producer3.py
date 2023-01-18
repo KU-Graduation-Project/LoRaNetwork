65fcb9a01cbef01c59dcfab8f0b76b2243c11057
kksks3192@gmail.com <kksks3192@gmail.com>
2023-01-18 11:50:22 +0900

    mqtt producer topic seperate

--- src/mqtt-kafka/mqtt_producer3.py
+++ src/mqtt-kafka/mqtt_producer3.py
@@ -10,7 +10,7 @@ mqtt_client.connect(mqtt_broker_address)
 while True:
         randNumber = uniform(20.0, 21.0)
         # json으로 encode해서 publish
-        mqtt_client.publish("test2", json.dumps({"user": "test3", "data": randNumber}), 1)
+        mqtt_client.publish("test3", json.dumps({"user": "test3", "data": randNumber}), 1)
         print('test3_MQTT : published ' + str(randNumber))
 
         time.sleep(4)
--- src/mqtt-kafka/mqtt_producer4.py
+++ src/mqtt-kafka/mqtt_producer4.py
@@ -10,7 +10,7 @@ mqtt_client.connect(mqtt_broker_address)
 while True:
         randNumber = uniform(20.0, 21.0)
         # json으로 encode해서 publish
-        mqtt_client.publish("test2", json.dumps({"user": "test4", "data": randNumber}), 1)
+        mqtt_client.publish("test4", json.dumps({"user": "test4", "data": randNumber}), 1)
         print('test4_MQTT : published ' + str(randNumber))
 
         time.sleep(4)
