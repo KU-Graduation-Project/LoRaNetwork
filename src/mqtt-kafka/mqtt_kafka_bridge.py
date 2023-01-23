2a1e6a708d018e8aabaf32b8109f55a8e16007e8
kksks3192@gmail.com <kksks3192@gmail.com>
2023-01-23 17:23:54 +0900

    multiprocess producer and consumer

--- src/mqtt-kafka/mqtt_kafka_bridge.py
+++ src/mqtt-kafka/mqtt_kafka_bridge.py
@@ -8,12 +8,18 @@ import time
 
 "mqtt 브로커에서 데이터 받아 user별로 카프카에 전달"
 
-#mosquitto연결
-mqtt_broker_address = '127.0.0.1'
-mqtt_client = mqtt.Client('MQTTConsumer')
-mqtt_client.connect(mqtt_broker_address)
 
-kafka_client = KafkaClient(hosts='localhost:9092')
+def connect(topic):
+    # mosquitto연결
+    mqtt_broker_address = '127.0.0.1'
+    mqtt_client = mqtt.Client(topic+'_MQTTConsumer')
+    mqtt_client.connect(mqtt_broker_address)
+    mqtt_topic = topic
+
+    if mqtt_client.subscribe(mqtt_topic):
+        print('MQTT Listener open :' + mqtt_topic)
+    mqtt_client.on_message = on_message
+    mqtt_client.loop_forever()
 
 
 def on_message(client, userdata, message):
@@ -21,28 +27,22 @@ def on_message(client, userdata, message):
     m_in = json.loads(m_decode)
     topic = m_in["user"]
 
+    kafka_client = KafkaClient(hosts='localhost:9092')
     kafka_topic = kafka_client.topics[topic]  # 토픽 지정
     kafka_producer = kafka_topic.get_sync_producer()
 
     msg_payload = str(message.payload)
-    print('MQTT message ', msg_payload)
+    print('MQTT in - ', msg_payload)
     kafka_producer.produce(str(msg_payload).encode('ascii'))
-    print('KAFKA : published ' + str(msg_payload) + 'to ' + topic)
+    print('KAFKA out - ' + str(msg_payload) + ' to ' + topic)
 
-
-def on_client(mqtt_topic):
-    mqtt_client.loop_start()
-    print('MQTT Listener open :', mqtt_topic)
-    mqtt_client.subscribe(mqtt_topic)  # 토픽 지정
-    mqtt_client.on_message = on_message
-    time.sleep(400)
-    mqtt_client.loop_stop()
 
 user_list = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"]
 
+
 if __name__=='__main__':
     # 컨슈머 멀티프로세싱
-    pool = multiprocessing.Pool(processes=10)
-    pool.map(on_client, user_list)
+    pool = multiprocessing.Pool(processes=3)
+    pool.map(connect, user_list)
 
 
--- src/mqtt-kafka/mqtt_producer.py
+++ src/mqtt-kafka/mqtt_producer.py
@@ -2,27 +2,48 @@ import json
 import multiprocessing
 
 import paho.mqtt.client as mqtt
-from random import uniform
+from random import randint
 import time
 from datetime import datetime
 
-mqtt_broker_address = '127.0.0.1'
-mqtt_client = mqtt.Client('MQTTProducer')
-mqtt_client.connect(mqtt_broker_address)
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        global Connected
+        Connected = True
+    else:
+        print("Connection failed")
+
+
+def client_conn():
+    # MQTT connection
+    cli = "producer" + str(datetime.now())
+    mqtt_client = mqtt.Client(cli)
+    mqtt_client.on_connect = on_connect
+
+    mqtt_broker_address = '127.0.0.1'
+    mqtt_client.connect(mqtt_broker_address, 1883)
+    mqtt_client.loop_start()
+    #while Connected != True:
+    #    time.sleep(0.1)
+    return mqtt_client
+
+
 
 def make_producer(user) :
-    while True :
-        randNumber = uniform(20.0, 21.0)
+    mqtt_client = client_conn()
+    while True:
+        randNumber = randint(0, 360)
         timestamp = datetime.now()
         # json으로 encode해서 publish
-        mqtt_client.publish(user, json.dumps({"user": user, "timestamp": str(timestamp), "data": randNumber}), 1)
-        print(user+'_MQTT published : ' + str(timestamp) +' - ' + str(randNumber))
+        if mqtt_client.publish(user, json.dumps({"user": user, "timestamp": str(timestamp), "data": randNumber}), 1):
+            print(user+'_MQTT published : ' + str(timestamp) +' - ' + str(randNumber))
 
         time.sleep(4)
 
 user_list = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"]
 
+
 if __name__=='__main__':
     # 컨슈머 멀티프로세싱
-    pool = multiprocessing.Pool(processes=10)
+    pool = multiprocessing.Pool(processes=3)
     pool.map(make_producer, user_list)
