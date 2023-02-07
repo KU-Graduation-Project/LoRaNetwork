e12630a598fd81b47bec3326edbb294c782bb2b6
kksks3192@gmail.com <kksks3192@gmail.com>
2023-02-07 21:19:09 +0900

    feat: motionDetect(UpDown_LeftRight) serial monitor

--- src/MotionDetectAdvertising/MotionDetectAdvertising.ino
+++ src/MotionDetectAdvertising/MotionDetectAdvertising.ino
@@ -7,16 +7,24 @@
 #define MAX_ACCEPTED_RANGE  2.0f
 
 LSM6DS3 myIMU(I2C_MODE, 0x6A);
+static bool debug_nn = false;
 
 void setup() {
   
     Serial.begin(9600);
+    digitalWrite(LED_BUILTIN, HIGH);
     while (!Serial);
     //Call .begin() to configure the IMUs
-    if (myIMU.begin() != 0) {
-        ei_printf("Device error");
+
+    if (myIMU.begin()) {
+      ei_printf("IMU initialized\r\n");
     } else {
-        ei_printf("Device OK!");
+      //ei_printf("Failed to initialize IMU\r\n");
+    }
+ 
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 7) {
+        ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 7 (the 7 sensor axes)\n");
+        return;
     }
 
 
@@ -27,11 +35,8 @@ float ei_get_sign(float number) {
 }
 
 void loop() {
-    ei_printf("\nStart Detecting in 2 seconds...\n");
- 
-    delay(2000);
- 
-    ei_printf("Sampling...\n");
+    
+    ei_printf("\nSampling...\n");
  
     // IMU데이터 저장을 위한 버퍼
     float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
@@ -48,19 +53,19 @@ void loop() {
         buffer[i+5] = myIMU.readFloatGyroZ();
         buffer[i+6] = myIMU.readTempC();
  
-        for (int j = 0; j < 7; j++) {
-            if (fabs(buffer[i + j]) > MAX_ACCEPTED_RANGE) {
-                buffer[i + j] = ei_get_sign(buffer[i + j]) * MAX_ACCEPTED_RANGE;
-            }
-        }
- 
+//        for (int j = 0; j < 7; j++) {
+//            if (fabs(buffer[i + j]) > MAX_ACCEPTED_RANGE) {
+//                buffer[i + j] = ei_get_sign(buffer[i + j]) * MAX_ACCEPTED_RANGE;
+//            }
+//        }
+// 
         buffer[i + 0] *= CONVERT_TO_MS2;
         buffer[i + 1] *= CONVERT_TO_MS2;
         buffer[i + 2] *= CONVERT_TO_MS2;
         buffer[i + 3] *= CONVERT_TO_MS2;
         buffer[i + 4] *= CONVERT_TO_MS2;
         buffer[i + 5] *= CONVERT_TO_MS2;
-        buffer[i + 5] *= CONVERT_TO_MS2;
+        buffer[i + 6] *= CONVERT_TO_MS2;
         
  
         delayMicroseconds(next_tick - micros());
@@ -71,11 +76,11 @@ void loop() {
     signal_t signal;
     int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
     if (err != 0) {
-        Serial.println("Failed to create signal from buffer (%d)\n", err);
+        ei_printf("Failed to create signal from buffer (%d)\n", err);
         return;
     }
  
-    // Run the classifier
+    // MotionDetect 시작
     ei_impulse_result_t result = { 0 };
  
     err = run_classifier(&signal, &result, debug_nn);
@@ -84,21 +89,23 @@ void loop() {
         return;
     }
 
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-      ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
-    }
 
-    if(result.classification[0].value > 0.5){
-      //UpDown
-      ei_printf("UpDown");
-      delay(2000);
-    }else if(result.classification[1].value > 0.5){
+    
+    //결과출력    
+    ei_printf("  %s: %.5f\n", result.classification[0].label, result.classification[0].value);
+    ei_printf("  %s: %.5f\n", result.classification[1].label, result.classification[1].value);
+    
+    if(0.1 < result.classification[0].value && result.classification[0].value < 0.5){
       //LeftRight
-      ei_printf("LeftRight");
-      delay(2000);      
+      ei_printf("---- LeftRight ----");
+      delay(3000);
+    }else if(result.classification[1].value > 0.87891){
+      //UpDown
+      ei_printf("----- UpDown  -----");
+      delay(3000);      
     }else{
       //idle
-      ei_printf("Idle");
-      delay(2000);
+      ei_printf("-----  Idle  -----");
+      delay(3000);
     }
 }
