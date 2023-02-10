35f8aa92e98dffd6dce57939e1e5cebb569da42e
kksks3192@gmail.com <kksks3192@gmail.com>
2023-02-10 13:51:55 +0900

    refactor: bLE advertise raw sensor data

--- src/SensorDataBLE/SensorDataBLE.ino
+++ src/SensorDataBLE/SensorDataBLE.ino
@@ -9,7 +9,12 @@ LSM6DS3 myIMU(I2C_MODE, 0x6A);
 static bool debug_nn = false;
 
 BLEService myService("fff0");
-BLEShortCharacteristic accelerometerCharacteristic_X("2101", BLERead | BLEBroadcast);
+BLEShortCharacteristic accelerometerCharacteristic_X("ffa1", BLERead | BLEBroadcast);
+BLEShortCharacteristic accelerometerCharacteristic_Y("ffa2", BLERead | BLEBroadcast);
+BLEShortCharacteristic accelerometerCharacteristic_Z("ffa3", BLERead | BLEBroadcast);
+BLEShortCharacteristic gyroscopeCharacteristic_X("ffb1", BLERead | BLEBroadcast);
+BLEShortCharacteristic gyroscopeCharacteristic_Y("ffb2", BLERead | BLEBroadcast);
+BLEShortCharacteristic gyroscopeCharacteristic_Z("ffb3", BLERead | BLEBroadcast);
 
 
 void setup() {
@@ -19,7 +24,17 @@ void setup() {
    BLE.setLocalName("Test sensor");
    
   myService.addCharacteristic(accelerometerCharacteristic_X);
+  myService.addCharacteristic(accelerometerCharacteristic_Y);
+  myService.addCharacteristic(accelerometerCharacteristic_Z);
+  myService.addCharacteristic(gyroscopeCharacteristic_X);
+  myService.addCharacteristic(gyroscopeCharacteristic_Y);
+  myService.addCharacteristic(gyroscopeCharacteristic_Z);
   accelerometerCharacteristic_X.writeValue(0);
+  accelerometerCharacteristic_Y.writeValue(0);
+  accelerometerCharacteristic_Z.writeValue(0);
+  gyroscopeCharacteristic_X.writeValue(0);
+  gyroscopeCharacteristic_Y.writeValue(0);
+  gyroscopeCharacteristic_Z.writeValue(0);
    
   BLE.addService(myService);
 
@@ -44,6 +59,19 @@ void loop() {
   }
   
    int16_t accelerometer_X = round(myIMU.readFloatAccelX() * 100.0);
+   int16_t accelerometer_Y = round(myIMU.readFloatAccelY() * 100.0);
+   int16_t accelerometer_Z = round(myIMU.readFloatAccelZ() * 100.0);
+   int16_t gyroscope_X = round(myIMU.readFloatGyroX() * 100.0);
+   int16_t gyroscope_Y = round(myIMU.readFloatGyroY() * 100.0);
+   int16_t gyroscope_Z = round(myIMU.readFloatGyroZ() * 100.0);
+
+   
    accelerometerCharacteristic_X.writeValue(accelerometer_X);
+   accelerometerCharacteristic_Y.writeValue(accelerometer_Y);
+   accelerometerCharacteristic_Z.writeValue(accelerometer_Z);
+   gyroscopeCharacteristic_X.writeValue(gyroscope_X);
+   gyroscopeCharacteristic_Y.writeValue(gyroscope_Y);
+   gyroscopeCharacteristic_Z.writeValue(gyroscope_Z);
+
 
 }
--- src/ble_receiver.py
+++ src/ble_receiver.py
@@ -1,33 +1,41 @@
 import asyncio
+import time
+from datetime import datetime
+
 from bleak import BleakClient
 
 #scan시 arduino라는 이름으로 인식될 수 있음
 address = "E69FDBAC-4750-BF6F-0C68-5646E82D36E3"
 
-read_write_charcteristic_uuid = "0000FFF1-0000-1000-8000-00805F9B34FB"
+#0000(****)-0000 부분의 UUID만 설정하면 됨(16bit->128bit 변환)
+accelerometerCharacteristic_X_uuid = "0000FFA1-0000-1000-8000-00805F9B34FB"
 
 
 async def run(address):
     async with BleakClient(address) as client:
         print('connected')
         services = await client.get_services()
-        # 서비스내에 있는 캐릭터리스틱 정보 보기
-        for service in services:
-            print(service)
-            print('\tuuid:', service.uuid)
-            print('\tcharacteristic list:')
-            for characteristic in service.characteristics:
-                print('\t\t', characteristic)
-                print('\t\tuuid:', characteristic.uuid)
-                print('\t\tdescription :', characteristic.description)
-                # ['write-without-response', 'write', 'read', 'notify']
-                print('\t\tproperties :', characteristic.properties)
 
-        # 읽기/쓰기 캐릭터리스틱 uuid를 이용해 데이터 읽기
-        # 해당 캐릭터리스틱의 속성에는 read가 존재해야만 읽기가 가능하다.
-        read_data = await client.read_gatt_char(read_write_charcteristic_uuid)
-        # 읽근 데이터 출력
-        print('read_data: ', read_data)
+        for service in services:
+            print("service:", service)
+            # print('\tuuid:', service.uuid)
+            # print('\tcharacteristic list:')
+
+            while True:
+                time.sleep(1)
+                now = datetime.now()
+                timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+                print(' ', timestamp)
+                for characteristic in service.characteristics:
+                    # print('\t', characteristic)
+                    print('\t', characteristic.uuid)
+                    # print('\t\tdescription :', characteristic.description)
+                    # ['write-without-response', 'write', 'read', 'notify']
+                    # print('\t\tproperties :', characteristic.properties)
+
+                    # characteristic uuid로 데이터 읽기(characteristic 속성에 read가 존재해야 가능)
+                    data = int.from_bytes(await client.read_gatt_char(characteristic.uuid), byteorder='little', signed=True)
+                    print('\t data: ', data)
 
     print('disconnect')
 
