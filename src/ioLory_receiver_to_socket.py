9549387e72268f5ce4b2ac5a1b56775cc6695925
kksks3192@gmail.com <kksks3192@gmail.com>
2023-06-07 16:17:48 +0900

    ioLory user_info send to raspberry pi

--- src/ioLory_receiver_to_socket.py
+++ src/ioLory_receiver_to_socket.py
@@ -7,8 +7,7 @@ import socket
 import serial
 import struct
 
-
-# import mysql.connector
+import mysql.connector
 
 
 # ioLory receiver(COM5)
@@ -29,26 +28,22 @@ def make_port(port_name):
     return ser
 
 
-'''
 # open socket client
 # send data to web
 Host = '127.0.0.1'
-Port = 9999
+Port = 8080
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((Host, Port))
-'''
 
 # Running Port
-serial_port = make_port('COM5')
+serial_port = make_port('COM4')
 
-'''
 conn = mysql.connector.connect(host='localhost',
                                database='oceanlab',
-                               user='admin',
+                               user='root',
                                password='12341234')
 cur = conn.cursor()
-'''
 
 
 # Receiving Data, Thread 1, this function read byte data from serial port and save in datalist
@@ -63,11 +58,12 @@ def receive_data(serial_port):
         res = serial_port.readline()
 
         print("receive data: ", timestamp, " / ", res)
-        # client_socket.sendall(res)
+        client_socket.sendall(res)
     return
 
 
 isConnected = False
+
 
 # uLory(Raspberry Pi)-ioLory(Monitor system) initial connect
 # ioLory DID must be set as uLory sender
@@ -81,7 +77,7 @@ def conn_ack():
             print("received:", data)
             ack_msg = 'conn_ack'
             msg = ack_msg.encode('utf-8')
-            while data !='info_req':
+            while data != 'info_req':
                 print("sent:", msg)
                 time.sleep(0.6)
                 serial_port.write(msg)
@@ -99,6 +95,8 @@ while True:
         break
 
 isInfoSet = False
+
+
 # get user info from db
 # send back to uLory sender
 def send_user_info():
@@ -106,31 +104,30 @@ def send_user_info():
     if serial_port.readable():
         res = serial_port.readline()
         data = res.decode('utf-8')
+        if "-2, " in data:
+            isInfoSet = True
         if data == 'info_req':
             print("received:", data)
-            while data !='info_set':
-                '''
-                cur.execute("SELECT * FROM user")
-                data = str(cur.fetchall())
+            while data != 'info_set':
 
-                for i in range(0, data.size()):
-                    serial_port.write(data)
+                cur.execute("SELECT * FROM user")
+                user_data = "info_ack" + str(cur.fetchall())
+                encoded_user_data = user_data.encode('utf-8')
+                print("sent:", encoded_user_data)
+                serial_port.write(encoded_user_data)
+
                 '''
                 # user_info = "{{1, 01, one}, {2, 02, two}, {3, 03, three}, {4, 04, four}, {5, 05, five}, {6, 06, six}, {7, 07, seven}, {8, 08, eight}, {9, 09, nine}}"
                 user_info = "info_ack{{1, 01, one}, {2, 02, two}, {3, 03, three}, {4, 04, four}, {5, 05, five}, {6, 06, six}}"
                 encoded_user_info = user_info.encode()
                 serial_port.write(encoded_user_info)
-                print("sent:", encoded_user_info)
+                '''
                 if serial_port.readable():
                     res = serial_port.readline()
                     data = res.decode()
                     print('received:', data)
                 time.sleep(1)
-            for i in range(0, 20):
-                msg = "info_set_complete"
-                encoded_msg = msg.encode()
-                serial_port.write(encoded_msg)
-            isInfoSet = True
+
 
 while True:
     if isInfoSet is False:
@@ -138,11 +135,10 @@ while True:
     if isInfoSet is True:
         break
 
-# while True:
-# ser.write(b'check serial data')
-# print(serial_port.readline())
+while True:
+    print(serial_port.readline())
 
-# receive_data(serial_port)
-# time.sleep(0.1)
+    receive_data(serial_port)
+    time.sleep(0.1)
 
-# client_socket.close()
+client_socket.close()
