fb256c6dbb73e77b2b87e9b6591d1690050a1c43
Kaya <kksks3192@gmail.com>
2023-06-09 15:15:51 +0900

    ioLory_receiver_to_socket

--- src/ioLory_receiver_to_socket.py
+++ src/ioLory_receiver_to_socket.py
@@ -7,7 +7,7 @@ import socket
 import serial
 import struct
 
-import mysql.connector
+#import mysql.connector
 
 
 # ioLory receiver(COM5)
@@ -27,7 +27,10 @@ def make_port(port_name):
 
     return ser
 
+# Running Port
+serial_port = make_port('COM5')
 
+'''
 # open socket client
 # send data to web
 Host = '127.0.0.1'
@@ -36,15 +39,12 @@ Port = 8080
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((Host, Port))
 
-# Running Port
-serial_port = make_port('COM4')
-
 conn = mysql.connector.connect(host='localhost',
                                database='oceanlab',
                                user='root',
                                password='12341234')
 cur = conn.cursor()
-
+'''
 
 # Receiving Data, Thread 1, this function read byte data from serial port and save in datalist
 # ser : serial port
@@ -58,10 +58,10 @@ def receive_data(serial_port):
         res = serial_port.readline()
 
         print("receive data: ", timestamp, " / ", res)
-        client_socket.sendall(res)
+        #client_socket.sendall(res)
     return
 
-
+'''
 isConnected = False
 
 
@@ -93,9 +93,9 @@ while True:
     if isConnected is True:
         print("conn_ack break")
         break
+'''
 
 isInfoSet = False
-
 
 # get user info from db
 # send back to uLory sender
@@ -104,29 +104,29 @@ def send_user_info():
     if serial_port.readable():
         res = serial_port.readline()
         data = res.decode('utf-8')
-        if "-2" in data:
+        if "-" in data:
+            print("userinfo break:", data)
             isInfoSet = True
         if data == 'info_req':
             print("received:", data)
-            while data != 'info_set':
+            '''
+            cur.execute("SELECT * FROM user")
+            user_info = "info_ack" + str(cur.fetchall())
+            encoded_user_info = user_data.encode('utf-8')
+            print("sent:", encoded_user_info)
+            serial_port.write(encoded_user_info)
 
-                cur.execute("SELECT * FROM user")
-                user_data = "info_ack" + str(cur.fetchall())
-                encoded_user_data = user_data.encode('utf-8')
-                print("sent:", encoded_user_data)
-                serial_port.write(encoded_user_data)
+            '''
 
-                '''
-                # user_info = "{{1, 01, one}, {2, 02, two}, {3, 03, three}, {4, 04, four}, {5, 05, five}, {6, 06, six}, {7, 07, seven}, {8, 08, eight}, {9, 09, nine}}"
-                user_info = "info_ack{{1, 01, one}, {2, 02, two}, {3, 03, three}, {4, 04, four}, {5, 05, five}, {6, 06, six}}"
-                encoded_user_info = user_info.encode()
-                serial_port.write(encoded_user_info)
-                '''
-                if serial_port.readable():
-                    res = serial_port.readline()
-                    data = res.decode()
-                    print('received:', data)
-                time.sleep(1)
+            user_info = "info_ack[('1', '01', 'one'), ('2', '02', 'two'), ('5', '05', 'five'), ('6', '06', 'six')]"
+            encoded_user_info = user_info.encode()
+            print("sent:", encoded_user_info)
+            serial_port.write(encoded_user_info)
+
+            if serial_port.readable():
+                res = serial_port.readline()
+                data = res.decode()
+                print('received:', data)
 
 
 while True:
@@ -141,4 +141,4 @@ while True:
     receive_data(serial_port)
     time.sleep(0.1)
 
-client_socket.close()
+#client_socket.close()
